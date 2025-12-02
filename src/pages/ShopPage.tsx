ffd971fe99f7765dce275129f8fbd6f0b90273ea
Mahmoud Nassim <mahmodnassim7@gmail.com>
2025-12-02 03:51:55 +0200

    add Category component

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,7 +1,8 @@
 import HeroSectionComponent from "../components/HeroSectionComponent";
 import bg from "../assets/images/Shop/bg-shop.jpg";
-import ProductComponent from "../components/Shop/productComponent";
+import ProductComponent from "../components/Shop/ProductComponent";
 import Cart from "../components/Shop/Cart";
+import Categories from "../components/Shop/Categories";
 const ShopPage = () => {
   return (
     <div>
@@ -11,9 +12,12 @@ const ShopPage = () => {
         title="shop"
         subtitle="- enjoy shopping for coffee -"
       />
-      <div className="flex flex-row max-md:flex-col justify-center gap-10 my-10 container mx-auto">
+      <div className="flex flex-row max-md:flex-col justify-center gap-5 my-10 container mx-auto p-2">
         <ProductComponent />
-        <Cart />
+        <div className="flex flex-col gap-6">
+          <Cart />
+          <Categories />
+        </div>
       </div>
     </div>
   );
